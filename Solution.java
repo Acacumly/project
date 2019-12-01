ecddee9cf0606d26e5cf4acd018d4315ef81a664
Acacumly <985514515@qq.com>
2019-12-01 12:17:53 +0800

    统计每个月兔子的总数

---
+++ Solution.java
@@ -0,0 +1,20 @@
+package Practice;
+import java.util.*;
+
+public class Solution{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            int monthCount = in.nextInt();
+            System.out.println(getTotalCount(monthCount));
+        }
+        in.close();
+    }
+
+    public static int getTotalCount(int monthCount){
+        if(monthCount == 1 || monthCount == 2){
+            return 1;
+        }
+        return getTotalCount(monthCount - 1) + getTotalCount(monthCount - 2);
+    }
+}
