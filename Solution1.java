d6819d80619cb972122b935fe79fe0cb56fd71fe
Acacumly <985514515@qq.com>
2019-12-02 20:46:29 +0800

    二叉树平衡检查

---
+++ Solution1.java
@@ -0,0 +1,37 @@
+package Practice;
+import java.util.*;
+
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+public class Solution1 {
+    public boolean isBalance(TreeNode root) {
+        // write code here
+        //二叉树平衡检查
+        if(root == null){
+            return true;
+        } else{
+            int left = getDeep(root.left);
+            int right = getDeep(root.right);
+            if(Math.abs(left - right) <= 1){
+                return isBalance(root.left)&&isBalance(root.right);
+            }else {
+                return false;
+            }
+        }
+    }
+    public int getDeep(TreeNode root){
+        if(root != null){
+            int left = getDeep(root.left);
+            int right = getDeep(root.right);
+            return left > right ? left + 1 : right +1;
+        }else{
+            return 0;
+        }
+    }
+}
